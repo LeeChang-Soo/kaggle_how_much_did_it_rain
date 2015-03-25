e1854145c1241e954f97f1aa4bf212c7e15a58ed
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-24 21:44:32 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -4,8 +4,8 @@ scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_how
 tar zxvf how_much_did_it_rain.tar.gz
 rm how_much_did_it_rain.tar.gz
 
-./split_csv.py > output.out 2> output.err
-./my_model.py >> output.out 2>> output.err
+# ./split_csv.py > output.out 2> output.err ### do this beforehand...
+./my_model.py > output.out 2> output.err
 
 # D=`date +%Y%m%d%H%M%S`
 # tar zcvf output_${D}.tar.gz model.pkl.gz output.out output.err
