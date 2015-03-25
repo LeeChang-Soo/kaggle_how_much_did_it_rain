49c54465e8738734fab9db612ec96fe350a7f4d5
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-25 15:01:39 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -6,10 +6,10 @@ rm how_much_did_it_rain.tar.gz
 
 # ./split_csv.py > output.out 2> output.err ### do this beforehand...
 # ./load_data.py > output.out 2> output.err 
-./my_model.py > output.out 2> output.err
+./my_model.py 0 69 > output.out 2> output.err
 
 # D=`date +%Y%m%d%H%M%S`
 # tar zcvf output_${D}.tar.gz model.pkl.gz output.out output.err
-# scp output_${D}.tar.gz ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_how_much_did_it_rain/
+scp model_*.pkl.gz ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_how_much_did_it_rain/
 ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk done_kaggle_how_much_did_it_rain"
 echo "JOB DONE how_much_did_it_rain"
