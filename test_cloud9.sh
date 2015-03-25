03d9b10aebbab7401f42abaa898591c78b8206ac
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-24 21:13:05 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -4,10 +4,11 @@ scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_how
 tar zxvf how_much_did_it_rain.tar.gz
 rm how_much_did_it_rain.tar.gz
 
-./my_model.py $1 > output_${1}.out 2> output_${1}.err
+./split_csv.py > output.out 2> output.err
+./my_model.py >> output.out 2>> output.err
 
-D=`date +%Y%m%d%H%M%S`
-tar zcvf output_${1}_${D}.tar.gz model_${1}.pkl.gz output_${1}.out output_${1}.err
-scp output_${1}_${D}.tar.gz ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_how_much_did_it_rain/
-ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk done_kaggle_how_much_did_it_rain_${1}"
-echo "JOB DONE ${1}"
+# D=`date +%Y%m%d%H%M%S`
+# tar zcvf output_${D}.tar.gz model.pkl.gz output.out output.err
+# scp output_${D}.tar.gz ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_how_much_did_it_rain/
+ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk done_kaggle_how_much_did_it_rain"
+echo "JOB DONE how_much_did_it_rain"
