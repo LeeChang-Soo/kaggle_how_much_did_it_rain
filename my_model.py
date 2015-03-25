a19d6bc6aee32057cf8f471f6d1d9d93f5448462
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-24 21:52:46 -0400

    ...

--- my_model.py
+++ my_model.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
 
 import os
+import gzip
+
+import cPickle as pickle
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.cross_validation import train_test_split
 
 from load_data import load_data
 
-from sklearn.ensemble import RandomForestRegressor
-
-from sklearn import cross_validation
-
 def score_model(model, xtrain, ytrain):
     randint = reduce(lambda x,y: x|y, [ord(x)<<(n*8) for (n,x) in enumerate(os.urandom(4))])
-    xTrain, xTest, yTrain, yTest = cross_validation.train_test_split(xtrain,
-                                                                     ytrain,
-                                                                     test_size=0.4, random_state=randint)
+    xTrain, xTest, yTrain, yTest = train_test_split(xtrain, ytrain,
+                                                    test_size=0.4,
+                                                    random_state=randint)
     model.fit(xTrain, yTrain)
     print model.score(xTest, yTest)
-    #cvAccuracy = np.mean(cross_val_score(model, xtrain, ytrain, cv=2))
-    #ytest_pred = model.predict(xTest)
-    #print 'rmsle', calculate_rmsle(ytest_pred, yTest)
+    
+    with gzip.open('model.pkl.gz' % index, 'wb') as mfile:
+        pickle.dump(model, mfile, protocol=2)
 
 if __name__ == '__main__':
     xtrain, ytrain, xtest, ytest = load_data()
