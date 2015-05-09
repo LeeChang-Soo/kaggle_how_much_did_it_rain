1df4580714f1a5a387b546d7b677631eeaa37380
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-09 05:37:23 -0400

    GradientBoostingClassifier tuning

--- my_model.py
+++ my_model.py
@@ -12,15 +12,27 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import SGDClassifier
 
+from sklearn.grid_search import GridSearchCV
+
 from sklearn.cross_validation import train_test_split
 
 from load_data import load_data
 
-def score_model_parallel(model, xtrain, ytrain, yvalue=0):
+def score_model_parallel(model, xtrain, ytrain, yvalue=0, do_grid_search=False):
     randint = reduce(lambda x,y: x|y, [ord(x)<<(n*8) for (n,x) in enumerate(os.urandom(4))])
     xTrain, xTest, yTrain, yTest = train_test_split(xtrain, (ytrain <= yvalue).astype(int),
                                                     test_size=0.4,
                                                     random_state=randint)
+    n_est = [10, 20]
+    m_dep = [2, 3, 4, 5, 6, 7, 10]
+
+    if do_grid_search:
+        model = GridSearchCV(estimator=model,
+                                    param_grid=dict(n_estimators=n_est,
+                                                    max_depth=m_dep),
+                                    scoring=scorer,
+                                    n_jobs=-1, verbose=1)
+
     model.fit(xTrain, yTrain)
     print model.score(xTest, yTest)
 
@@ -68,11 +80,12 @@ if __name__ == '__main__':
 
     if begin_idx < 70:
         #model = SGDClassifier(loss='log', n_jobs=-1, penalty='l1', verbose=1, n_iter=200)
-        #model = GradientBoostingClassifier(loss='deviance', verbose=1)
-        model = RandomForestClassifier(n_estimators=10, n_jobs=-1, verbose=1)
+        model = GradientBoostingClassifier(loss='deviance', verbose=1)
+#        model = RandomForestClassifier(n_estimators=10, n_jobs=-1, verbose=1)
 
 
         for idx in range(begin_idx, end_idx+1):
-            score_model_parallel(model, xtrain, ytrain, yvalue=idx)
+            score_model_parallel(model, xtrain, ytrain, yvalue=idx,
+                                 do_grid_search=True)
     elif begin_idx == 70:
         create_submission_parallel(xtest, ytest)
